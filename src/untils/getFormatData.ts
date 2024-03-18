a5836fcf2723611590559d838339461e26ea4b6c
Kristina Manatskay <manackaakristina@gmail.com>
2024-03-18 15:22:19 +0300

    feat: add function to get date format

---
+++ src/untils/getFormatData.ts
@@ -0,0 +1,21 @@
+export const getFormatDate = (birthdate: string) => {
+  const date = new Date(birthdate);
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  const day = date.getDate();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+  return `${day} ${month} ${year}`;
+};
